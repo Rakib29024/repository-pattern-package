19281a82241fc990febc99b7639bdf888b34dd55
Rakib Hassan <rakib@piit.local>
2021-06-03 12:30:36 +0600

    config removed

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -19,9 +19,7 @@ class ServiceProvider extends LaravelServiceProvider
      */
     public function boot()
     {
-      $this->publishes([
-        __DIR__ . '/config/repository.php' => config_path('repository.php'),
-        ], 'repository');
+
     }
 
     /**
